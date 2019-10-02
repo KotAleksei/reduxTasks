72f25809647b2eba85d0950380dfbce1ff273fb3
cherrykot <kot.aleksei43@gmail.com>
2019-10-02 10:13:29 +0300

    Updates

---
+++ precache-manifest.2c056cdedd333c7aad694b5b38202f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6feb5ec6686893396f714200c739057",
+    "url": "/reduxTasks/index.html"
+  },
+  {
+    "revision": "3f00df42bd211de6002a",
+    "url": "/reduxTasks/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "611b6017bcb68cabb08e",
+    "url": "/reduxTasks/static/js/2.ea45b585.chunk.js"
+  },
+  {
+    "revision": "3f00df42bd211de6002a",
+    "url": "/reduxTasks/static/js/main.3eaae16d.chunk.js"
+  },
+  {
+    "revision": "f7b775d1b28e3306d3ec",
+    "url": "/reduxTasks/static/js/runtime-main.7d452f80.js"
+  }
+]);
